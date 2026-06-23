073aabc8d3d78d4e251bf11545190f527b37a93f
Ibrahim Nasir Muhtar <khaleelibrahim364@gmail.com>
2026-06-23 15:47:56 +0100

    update debug

--- public/debug.php
+++ public/debug.php
@@ -1,15 +1,15 @@
 <?php
-$logPath = __DIR__ . '/../storage/logs/laravel.log';
-if (!file_exists($logPath)) {
-    echo "No log file found at $logPath";
-} else {
-    echo "<pre>";
-    $lines = @file($logPath);
-    if ($lines === false) {
-        echo "Could not read log file. Check permissions. Try running: chmod -R 775 storage";
-    } else {
-        $lastLines = array_slice($lines, -100);
-        echo htmlspecialchars(implode("", $lastLines));
-    }
-    echo "</pre>";
+$packagesPath = __DIR__ . '/../bootstrap/cache/packages.php';
+$servicesPath = __DIR__ . '/../bootstrap/cache/services.php';
+
+if (file_exists($packagesPath)) {
+    unlink($packagesPath);
+    echo "Deleted bootstrap/cache/packages.php<br>";
 }
+
+if (file_exists($servicesPath)) {
+    unlink($servicesPath);
+    echo "Deleted bootstrap/cache/services.php<br>";
+}
+
+echo "Cache cleared! You can now visit your homepage.";
